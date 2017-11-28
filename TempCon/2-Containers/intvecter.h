fe43a666b1f9c7ed9ad07c0af58191c9be61dc33
Hilitrium <edfbugkiller13@gmail.com>
2017-11-27 17:39:44 -0800

    Added 2-Containers

---
+++ TempCon/2-Containers/intvecter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+class intVector {
+	int * data;
+	size_t capacity;
+	size_t size;
+
+public:
+	intVector();
+	~intVector();
+
+	int& at(size_t idx);
+
+	int& append(int val);
+	
+	int* c_ptr() const;
+	size_t getCapacity() const;
+	size_t getSize() const;
+	
+	bool empty() const;
+
+	int front() const;
+	int back() const;
+
+private:
+	bool grow(size_t minSize);
+};
