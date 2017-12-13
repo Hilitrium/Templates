828e338665ca873d338b9a040f12f960538be55b
Hilitrium <edfbugkiller13@gmail.com>
2017-12-12 17:37:41 -0800

    continued work on HashMap.h

--- TempCon/2-Containers/HashMap.h
+++ TempCon/2-Containers/HashMap.h
@@ -13,6 +13,11 @@ class hashMap {
 public:
 	hashMap();
 	~hashMap();
+	size_t hash(size_t key);
+	void addHash(size_t key);
+	void deleteHash(size_t Hash);
+	void clearMap();
+	size_t atHash(size_t hash);
 
 private:
 	tVector arr;
@@ -22,4 +27,40 @@ template<typename T>
 inline hashMap<T>::hashMap() {
 	capacity = 50;
 	data = new T[capacity];
+	size = 0;
 }
+
+template<typename T>
+inline hashMap<T>::~hashMap() {
+	delete[] data;
+}
+
+template<typename T>
+inline size_t hashMap<T>::hash(size_t key) {
+	size_t hash = key / 2;
+	return hash;
+}
+
+template<typename T>
+inline void hashMap<T>::addHash(size_t key) {
+	size_t hash = hash(key);
+	arr[hash] = key;
+}
+
+template<typename T>
+inline void hashMap<T>::deleteHash(size_t Hash) {
+
+}
+
+template<typename T>
+inline void hashMap<T>::clearMap() {
+	size = 0;
+}
+
+template<typename T>
+inline size_t hashMap<T>::atHash(size_t hash)
+{
+	return size_t();
+}
+
+
